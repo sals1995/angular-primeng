6e706e510552b5397bb9c62c0bf6a9842b39379c
sals1995 <58127260+sals1995@users.noreply.github.com>
2022-09-06 04:01:27 +0200

    getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  dateSelected: Date;
+  // rangeDates: Date[];
+
+  // minDate: Date;
+
+  // maxDate: Date;
+
+  // invalidDates: Array<Date>
+
+  ngOnInit() {
+      // let today = new Date();
+      // let month = today.getMonth();
+      // let year = today.getFullYear();
+      // let prevMonth = (month === 0) ? 11 : month -1;
+      // let prevYear = (prevMonth === 11) ? year - 1 : year;
+      // let nextMonth = (month === 11) ? 0 : month + 1;
+      // let nextYear = (nextMonth === 0) ? year + 1 : year;
+      // this.minDate = new Date();
+      // this.minDate.setMonth(prevMonth);
+      // this.minDate.setFullYear(prevYear);
+      // this.maxDate = new Date();
+      // this.maxDate.setMonth(nextMonth);
+      // this.maxDate.setFullYear(nextYear);
+
+      // let invalidDate = new Date();
+      // invalidDate.setDate(today.getDate() - 1);
+      // this.invalidDates = [today,invalidDate];
+    }
+    selectingDate(){
+    console.log(this.dateSelected)
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RippleModule } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
+import {  CalendarModule } from 'primeng/calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,RippleModule,ButtonModule],
+  imports:      [ BrowserModule, FormsModule ,RippleModule,ButtonModule,CalendarModule,BrowserAnimationsModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
